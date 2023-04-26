57bc30f0a835c4ffea910c89f0aeb34ce7069363
ShekharSmohol <130433028+ShekharSmohol@users.noreply.github.com>
2023-04-26 15:01:08 +0530

    Created main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+#*                 Root Module                         *#
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+# Terraform Block
+
+terraform {
+    required_providers  {
+        azurerm =   {
+            source  =   "hashicorp/azurerm"
+        }
+    }
+}
+
+# Provider Block
+
+provider "azurerm" {
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
+}
