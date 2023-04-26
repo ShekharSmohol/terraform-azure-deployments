228d08909c9055834b3d7beeef6af2dcfce82bc9
ShekharSmohol <130433028+ShekharSmohol@users.noreply.github.com>
2023-04-26 18:08:15 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,21 +5,21 @@
 # Service Principal Variables
 
 variable "client_id" {
-    description =   "Client ID (APP ID) of the application"
+    description =   "2f348f9a-02f4-48a8-8f85-dfe551078126"
     type        =   string
 }
 
 variable "client_secret" {
-    description =   "Client Secret (Password) of the application"
+    description =   "xV48Q~ZKDrGTD3HGHg.0lawiXx8roMSDRBeUCduS"
     type        =   string
 }
 
 variable "subscription_id" {
-    description =   "Subscription ID"
+    description =   "d8b85040-7339-4dc0-910c-bf4a0c20de66"
     type        =   string
 }
 
 variable "tenant_id" {
-    description =   "Tenant ID"
+    description =   "e50ebc84-76f2-4636-b550-c3f7abc924af"
     type        =   string
 }
